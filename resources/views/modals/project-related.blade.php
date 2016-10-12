afd72bc76e4279552fa7f3b8c0fbad4a0defd709
rejtg21 <resmediodia@tmj.ph>
2016-10-12 20:51:07 +0800

    Remove max/min budget of column
    > add max/min of total budget of projects

--- resources/views/modals/project-related.blade.php
+++ resources/views/modals/project-related.blade.php
@@ -6,25 +6,35 @@
 @section('modal-content')
 	<!-- DTable -->
 	 <div ng-controller="ProjectRelatedController as prc" ng-init = 'prc.proj_id = submitData.proj_id; prc.refresh()'>
+
+ 		<div class = "form-group">
+ 			<div class = "row">
+ 				<label class = "col-md-4">Average Total Budget</label>
+ 				<label class = "col-md-4" ng-bind="prc.ave_budget"></label>
+ 			</div>
+ 		</div>
+ 		<div class = "form-group">
+ 			<div class = "row">
+ 				<label class = "col-md-4">Minimum Total Budget</label>
+ 				<label class = "col-md-4" ng-bind="prc.min_budget"></label>
+ 			</div>
+ 		</div>
+ 		<div class = "form-group">
+ 			<div class = "row">
+ 				<label class = "col-md-4">Maximum Total Budget</label>
+ 				<label class = "col-md-4" ng-bind="prc.max_budget"></label>
+ 			</div>
+ 		</div>
+
         <button class = "btn btn-danger pull-right" ng-click = "prc.refresh()">Refresh</button>
 
         <p class="text-danger"><strong>@{{ prc.message }}</strong></p>
         <br>
-		<div class = "form-group">
-			<div class = "row">
-				<label class = "col-md-4">Average Total Budget</label>
-				<div class = "col-md-4">
-					<input type = "text" readonly="" ng-model = "prc.ave_budget">
-				</div>
-			</div>
-		</div>
         <table datatable="ng" dt-options="prc.dtOptions" dt-columns="prc.dtColumnDefs"
         dt-instance="prc.dtInstance" class="table table-hover row-border hover">
         <thead>
             <tr>
               <th>Project Title</th>
-              <th>Minimum Budget</th>
-              <th>Maximum Budget</th>
               <th>Total Budget</th>
               <th>Duration</th>
               <th>Actions</th>
@@ -33,8 +43,6 @@
           <tbody>
             <tr ng-repeat = "data in prc.related">
               <td>@{{data.name }}</td>
-              <td>@{{data.min_budget}}</td>
-              <td>@{{data.max_budget}}</td>
               <td>@{{data.total_budget}}</td>
               <td>@{{data.duration}}</td>
               <td>
