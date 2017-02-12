4787f081298f869096462143cd027a0d8f2459ac
rejtg21 <resmediodia@tmj.ph>
2017-02-12 19:30:23 +0800

    commit for reference

--- resources/views/modals/projects.blade.php
+++ resources/views/modals/projects.blade.php
@@ -151,38 +151,32 @@
     </div>
 
     <!-- Expense -->
-    <div ng-if = "submitData.proj.id" ng-controller="ItemCtrl as ic">
-        <h3>Item/Expense</h3>
-        <hr ng-init = 'ic.proj_id = submitData.proj.id;ic.getProjItems()'>
+    <div ng-if = "submitData.proj.id" ng-controller="ProjectCategory as pc">
+        <h3>Budget</h3>
+        <hr ng-init = 'pc.proj_id = submitData.proj.id;pc.getProjItems()'>
         @if(Session::get('role') == config('constants.role_champion'))
-        <button class = "btn btn-sm btn-success pull-right" ng-click = "ic.add()"> Add Item/Expense</button>
+        <button class = "btn btn-sm btn-success pull-right" ng-click = "pc.add()"> Add Item/Expense</button>
         @endif
-        <button class = "btn btn-sm btn-danger pull-right" ng-click = "ic.getProjItems()"> Refresh</button>
+        <button class = "btn btn-sm btn-danger pull-right" ng-click = "pc.getProjItems()"> Refresh</button>
 
-        <p class="text-danger"><strong>@{{ ic.message }}</strong></p>
+        <p class="text-danger"><strong>@{{ pc.message }}</strong></p>
         <br>
-        <table datatable="ng" dt-options="ic.dtOptions" dt-columns="ic.dtColumnDefs" dt-instance="ic.dtInstance" class="table table-hover row-border hover">
+        <table datatable="ng" dt-options="pc.dtOptions" dt-columns="pc.dtColumnDefs" dt-instance="pc.dtInstance" class="table table-hover row-border hover">
         <thead>
             <tr>
-              <th>Item Name</th>
               <th>Category</th>
-              <th>Quantity</th>
-              <th>Price</th>
-              <th>Total Amount</th>
+              <th>Amount</th>
               <th>Actions</th>
             </tr>
           </thead>
           <tbody>
-            <tr ng-repeat = "data in ic.items">
-              <td>@{{data.item_name}}</td>
+            <tr ng-repeat = "data in pc.budget">
               <td>@{{data.category}}</td>
-              <td>@{{data.quantity}} (@{{data.quantity_label}})</td>
-              <td>@{{data.price}}</td>
-              <td>@{{ ((data.price) * (data.quantity)) }}</td>
+              <td>@{{data.amount}}</td>
               <td>
 
               @if(Session::get('role') == config('constants.role_finance'))
-                <button class="btn btn-warning btn-sm" ng-click="ic.edit($index, data)">
+                <button class="btn btn-warning btn-sm" ng-click="pc.edit($index, data)">
                 <i class="fa fa-edit"></i>
                 </button>
 			  @endif
