c0ccf5f45d536070c9134bf23fe6bfca6db48471
rejtg21 <resmediodia@tmj.ph>
2017-05-08 21:19:34 +0800

    modified progress report / project charter

--- resources/views/reports/project.blade.php
+++ resources/views/reports/project.blade.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 	<title>
-		Green School - Project
+		Green School - Project Charter Report
 	</title>
 
     {!! HTML::style('css/bootstrap/css/bootstrap.min.css'); !!}
@@ -16,7 +16,7 @@
 		color: white;
 	}
 	.sub-title {
-		text-align : center;
+		text-align : center !important;
 		width : 100% !important;
 	}
 	h4{
@@ -29,123 +29,142 @@
 	table thead th {
 		text-align:center;
 		width : 100%;
-		background-color: #2dcc70 !important;
+		/*background-color: #2dcc70 !important;*/
 		padding : 5px;
 	}
+  table {
+    /*background-color: red!important;*/
+  }
+  td {
+    /*background-color: blue!important;*/
+  }
+  .task-container {
+    margin-left:20px!important;
+  }
 </style>
-<table class = "table table-bordered table-responsive" width = "100%">
-	<thead>
+<table width = "100%">
 	<tr>
-		<th colspan = "4" class = "gspms-title"><h2>Green School</h2></th>
+		<td colspan= "4" class = "sub-title">
+			<h1><b>Project Charter Report</b></h1>
+      <br>
+      (Period: {{date('F Y', strtotime($proj->start_date))}} - {{date('F Y', strtotime($proj->end_date))}})
+    </td>
 	</tr>
-</thead>
-<tbody>
+  <tr>
+    <td colspan="4"><h4><u><b>Project Information</b></h4></u></td>
+  </tr>
+  <tr>
+    <td width="27%">Project Name:</td>
+    <td >{{$proj->name}}</td>
+    <td>Start Date:</td>
+    <td>{{date('m/d/Y', strtotime($proj->start_date))}}</td>
+  </tr>
 	<tr>
-		<td colspan=  "4" class = "sub-title">
-			<h3>Project Charter<h3></td>
-	</tr><!--
-	foreach($upd_arr['objective'] as $key => $value) {
-               if (empty($temp)) $temp .= $value;
-               else $temp .= $delimiter . $value;
-            } -->
-	<tr>
-		<td>Project Name</td>
-		<td>{{$proj->name}}</td>
-		<td>Program</td>
+		<td>Program :</td>
 		<td>{{$proj->program}}</td>
+    <td>End Date</td>
+    <td>{{date('m/d/Y', strtotime($proj->end_date))}}</td>
+	</tr>
+  {{-- <tr>
+    <td colspan="4"><br></td>
+  </tr> --}}
+	<tr>
+		<td>Partner Organization : </td>
+		<td >{{$proj->partner_organization}}</td>
 	</tr>
 	<tr>
-		<td>Start Date</td>
-		<td>{{date('F d, Y', strtotime($proj->start_date))}}</td>
-		<td>End Date</td>
-		<td>{{date('F d, Y', strtotime($proj->end_date))}}</td>
+    <td >Partner Community : </td>
+		<td >{{$proj->partner_community}}</td>
 	</tr>
-	<tr>
-		<td colspan=  "4" class = "sub-title">
-			<h4>Partners<h4></td>
-	</tr>
-	<tr>
-		<td>Organization</td>
-		<td>{{$proj->partner_organization}}</td>
-		<td>Community</td>
-		<td>{{$proj->partner_community}}</td>
-	</tr>
-	<tr>
-		<td>Champion</td>
+	<tr style ="padding-top:10px;">
+		<td>Champion : </td>
 		<td>{{$proj->champ_name}}</td>
-		<td>Resource Person</td>
-		<td>{{$proj->first_name . ' ' . $proj->middle_name . ' ' . $proj->last_name}}</td>
 	</tr>
-	<tr>
-		<td colspan = "2">Initial Budget</td>
-		<td colspan = "2">P {{number_format($proj->total_budget,2)}}</td>
-	</tr>
-	<tr>
-		<td colspan = "2">Total Budget Request</td>
-		<td colspan = "2">P {{number_format($total_budget_request,2)}}</td>
-	</tr>
-	<tr>
-		<td colspan = "2">Total Budget</td>
-		<td colspan = "2">P {{number_format($total_budget,2)}}</td>
-	</tr>
-	<tr>
-		<td colspan = "2">Total Expense</td>
-		<td colspan = "2">P {{number_format($total_expense,2)}}</td>
-	</tr>
-	<tr>
-		<td colspan= "4" class = "sub-title"><h4>Objectives<h4></td>
-	</tr>
-	<tr>
-		<td colspan = "4">
-			<?php
-			$delimiter = '(#$;)';
-			$proj->objective = explode($delimiter, $proj->objective);
-			?>
-			<ul class = "objectives">
-				@foreach($proj->objective as $key => $value)
-				<li>{{$value}}</li>
-				@endforeach
-			</ul>
-		</td>
-	</tr>
-	<tr>
-		<td colspan = "2">Total Work Done (TWD)</td>
-		<td colspan = "2">{{$duration}}</td>
-	</tr>
-	<tr>
-		<td colspan = "2">Budget at Completion (BAC) </td>
-		<td colspan = "2">{{number_format($proj->total_budget - $total_expense,2)}}</td>
-	</tr>
-	<tr>
-		<td colspan = "2">Problems / Remarks</td>
-		<td colspan = "2">{{ $proj->remarks }}</td>
-	</tr>
-	<!-- <tr>
-		<td colspan = "2">Earned Value (EV)</td>
-		<td colspan = "2"></td>
-	</tr> -->
-	<!-- <tr>
-		<td colspan = "2">Actual Cost (AC)</td>
-		<td colspan = "2">{{number_format($total_expense,2)}}</td>
-	</tr> -->
-	<!-- <tr>
-		<td colspan = "2">Cost Performance Index (CPI)</td>
-		<td colspan = "2"></td>
-	</tr>
-	<tr>
-		<td colspan = "2">Estimate at Completion (EAC)</td>
-		<td colspan = "2"></td>
-	</tr>
-	<tr>
-		<td colspan = "2">To-Complete Performance Index (TCPI)</td>
-		<td colspan = "2"></td>
-	</tr>
-	<tr>
-		<td colspan = "4">
-			<img src = "{{route('proj_chart', ['id' => 1 ])}}">
-		</td>
-	</tr> -->
-</tbody>
+  <tr>
+    <td  >Resource Person : </td>
+    <?php $fullname = $proj->first_name . ' ' . $proj->middle_name . ' ' . $proj->last_name; ?>
+    <td >{{trim($fullname) ?: 'No Resource Person'}}</td>
+  </tr>
+  <tr style = "padding-top:10px;">
+    <td colspan="4"><h4><u><b>Objectives</b></h4></u></td>
+  </tr>
+  <tr>
+    <td colspan = "4">
+      <?php
+      $delimiter = '(#$;)';
+      $proj->objective = explode($delimiter, $proj->objective);
+      ?>
+      <ul class = "objectives">
+        @foreach($proj->objective as $key => $value)
+        <li>{{$value}}</li>
+        @endforeach
+      </ul>
+    </td>
+  </tr>
+  <tr style = "padding-top:10px;">
+    <td colspan="4"><h4><u><b>Activities</b></h4></u></td>
+  </tr>
+  <tr>
+    <td>
+    <ul class = "objectives">
+      @foreach($activities as $key => $value)
+      <li>{{$value['name']}}</li>
+
+          &nbsp;&nbsp;&nbsp;&nbsp;<span style="font-weight:bold"><u>TASKS</u></span><br>
+          @foreach($value['tasks'] AS $val)
+            &nbsp;&nbsp;&nbsp;&nbsp;<span>- {{$val['name']}}</span><br>
+          @endforeach
+          @if(COUNT($value['tasks']) == 0)
+            &nbsp;&nbsp;&nbsp;&nbsp;<span>No task(s) found</span><br>
+          @endif
+      @endforeach
+    </ul>
+    </td>
+  </tr>
+  <tr style = "margin-top:20px;">
+    <td colspan="4"><h4><u><b>Milestones</b></h4></u></td>
+  </tr>
+  <tr>
+
+  </tr>
+  <tr style = "padding-top:10px;">
+    <td colspan="4"><h4><u><b>Project Budget</b></h4></u></td>
+  </tr>
+  <tr>
+    <td>Initial Budget: </td>
+    <td colspan="3"> P {{number_format($proj->total_budget,2)}}</td>
+  </tr>
+  <tr>
+    <td colspan="2">
+    <table class = "table table-bordered" style="padding:20px; width:50%">
+      <tr>
+        <th style="padding:10px;"><u> CATEGORY </u></th>
+        <th style="padding:10px;"><u> AMOUNT </u></th>
+      </tr>
+      @foreach($expenses as $val)
+      <tr>
+        <td>{{$val->category}}</td>
+        <td>{{number_format($val->amount,2,'.',',')}}</td>
+      </tr>
+      @endforeach
+      @if(COUNT($expenses) == 0)
+      <tr>
+        <td colspan="2">No Expense(s) Found</td>
+      </tr>
+      @endif
+      <tr>
+        <td> <b><u> TOTAL </u></b></td>
+        <td> P {{number_format($total_expense,2, '.',',')}}</td>
+      </tr>
+    </table>
+    </td>
+  </tr>
+  <tr>
+    <?php $user = auth()->user()->infos[0];
+          $fullName = $user->last_name . ', ' . $user->first_name . ' ' . $user->middle_name;
+          ?>
+    <td colspan="4" style="text-align:right">{{'Generated By: ' . ucwords($fullName) . ' | Dated: ' . date('F d, Y')}}</td>
+  </tr>
 </table>
     {!! HTML::script('js/others/jquery-1.12.0.min.js') !!}
     {!! HTML::script('plugins/bootstrap/js/bootstrap.min.js') !!}
